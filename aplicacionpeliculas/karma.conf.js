9f4b2c1c528d9d885ff850be418aeb2a40223b6d
Jesus Perez <j-perez2008@hotmail.com>
2022-10-29 02:12:25 -0400

    creaciondenuevo

--- aplicacionpeliculas/karma.conf.js
+++ aplicacionpeliculas/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/frontendpelicula'),
+      dir: require('path').join(__dirname, './coverage/aplicacionpeliculas'),
       subdir: '.',
       reporters: [
         { type: 'html' },
